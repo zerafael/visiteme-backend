c8e41e3e01541dc30af59544b104b70e909fae94
Leonel Pelusi <pelusi@localhost.localdomain>
2019-11-26 16:04:57 -0300

    Add StudentController

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,23 @@
-class StudentController {
-	async index(request, response) {}
+import Student from '../models/Student'
 
-	async store(request, response) {}
+class StudentController {
+	async index(request, response) {
+		const students = await Student.findAll();
+
+		return response.json(students);
+	}
+
+	async indexOne(request, response) {
+		const student = await Student.findOne({ where: { email: request.body.email } });
+
+		return response.json(student);
+	}
+
+	async store(request, response) {
+		const student = await Student.create(request.body);
+
+		return response.json(student);
+	}
 }
 
 export default new StudentController();
