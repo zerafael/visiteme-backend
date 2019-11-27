08b1b07fb6ff0a8601af04809dd85bcf4b211c77
José Rafael <zerafael1@gmail.com>
2019-11-26 23:27:26 -0300

    Add .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,17 @@
+require("dotenv/config");
+
 module.exports = {
 	dialect: "postgres",
-	host: "ec2-23-21-91-183.compute-1.amazonaws.com",
-	port: 5432,
-	username: "omyvlabpehwonk",
-	password: "9ffcbdefc1f6a0cec6f79baaaf750f4f1a3d423a9e6d438712a2403b392c6e99",
-	database: "ddq9vuo03a1h0a",
+	host: process.env.DB_HOST || "ec2-23-21-91-183.compute-1.amazonaws.com",
+	port: process.env.DB_PORT || 5432,
+	username: process.env.DB_USER || "omyvlabpehwonk",
+	password:
+		process.env.DB_PASS ||
+		"9ffcbdefc1f6a0cec6f79baaaf750f4f1a3d423a9e6d438712a2403b392c6e99",
+	database: process.env.DB_NAME || "ddq9vuo03a1h0a",
 	define: {
 		timestamps: true,
 		underscored: true,
 		underscoredAll: true
 	}
- };
- 
+};
